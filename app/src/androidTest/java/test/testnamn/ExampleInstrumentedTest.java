08113a85c00f3df01dee0f10d2b7044dc1cf3730
jackepp <jacob_permansson@hotmail.com>
2017-05-19 12:12:21 +0200

    ju

--- app/src/androidTest/java/test/testnamn/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/test/testnamn/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-        String hej = "ello";
+        String hej = "esdsddddddddllo";
         assertEquals("test.testnamn", appContext.getPackageName());
     }
 }
